c72999e9fdab6cb65e105dfaf749b1c3ecdc4ff8
sago007 <poul@poulsander.com>
2015-11-28 15:14:49 +0100

    Updated using deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,7 @@
 rsync -avPr -e ssh _site/ sago007@web.sf.net:/home/project-web/b/bl/blockattack/htdocs/
+cd ../blockattack.github.io/
+git pull
+rsync -r ../blockattack/_site/ ./
+git add .
+git commit -m "Updated using deploy script"
+
