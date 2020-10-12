52ea797f283e3a43d1deff50458567058c260eb4
Poul Sander <poul@poulsander.com>
2020-10-12 17:49:31 +0200

    Updated using deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-set +e
+set -e
 docker build . -t jekyll
-docker run -it --rm -v $(pwd):/workdir jekyl
+docker run -it --rm -v $(pwd):/workdir jekyll
 rsync -avPr -e ssh _site/ sago007@web.sf.net:/home/project-web/b/bl/blockattack/htdocs/
 cd ../blockattack.github.io/
 git pull
